7095c5a0417e109321d0f0ff5d8be8d88c4e0432
Akash Yadav <Akash.Yadav@asl.mum.sst>
2023-08-06 23:42:43 +0530

    final commit

--- Bank/Bank.js
+++ Bank/Bank.js
@@ -2,7 +2,7 @@ const UnauthorizedError = require("./Error/UnAuthorizedError");
 const NotFound = require("./Error/NotFound");
 const InvalidType = require("./Error/InvalidType");
 class Bank {
-  
+  static allBank = [];
   static ID = 1;
   constructor(name) {
     this.id = Bank.ID++;
@@ -10,47 +10,149 @@ class Bank {
     this.accounts = [];
   }
 
-  static newBank(isAdmin, name) {
+  static newBank(name) {
     try {
-      if (!isAdmin) {
-        throw new UnauthorizedError("Unauthorized");
-      }
-      if (typeof id != "number") {
-        throw new InvalidType(" Id should  number ");
-      }
-      if (typeof name != "string") {
-        throw new InvalidType(" Id should  String  ");
-      }
+      let bankObj = new Bank(name);
+      Bank.allBank.push(bankObj);
 
-      let bank = new Bank(name);
-
-       return bank
+      return bankObj;
     } catch (error) {
       throw error;
     }
   }
 
+  static findBankId(bankid) {
+    try {
+      if (typeof bankid != "number") {
+        throw new InvalidType("  invalid bankid");
+      }
 
-  getBankId(){
-    return this.id
+      for (let index = 0; index < Bank.allBank.length; index++) {
+        if (bankid == allBank[index]) {
+          return index;
+        }
+      }
+      throw new NotFound("bank id Not found ");
+    } catch (error) {
+      throw error;
+    }
+  }
+  static validateBankId(bankId) {
+    try {
+      if (typeof bankId != "number") {
+        throw new InvalidType("  invalid bankid");
+      }
+
+      for (let index = 0; index < Bank.allBank.length; index++) {
+        if (bankId == Bank.allBank[index].id) {
+          return true;
+        }
+      }
+
+      throw new NotFound("bank id Not found ");
+    } catch (error) {
+      throw error;
+    }
   }
 
+  updateBankDetails(bankid, parameter, newValue) {
+    try {
+      if (typeof parameter != "string") {
+        throw InvalidType(" Invalid parameter ");
+      }
 
-  updateBankName(bankName){
- try{
-   
-     if( typeof(bankName)!="string"){
-      throw InvalidType(" Invalid bankName format")
-     }
+      let indexOfbank = Bank.findBankId(bankid);
+
+      switch (parameter) {
+        case " name ":
+          if (typeof newValue != "string") {
+            throw new InvalidType("Invalid full name Format");
+          }
+
+        default:
+          throw new NotFound("Parameter not found");
+      }
+    } catch (error) {
+      throw error;
     }
- catch(error){
+  }
 
-   throw error
- }
+  getAllBank() {
+    try {
+      if (!this.isAdmin) {
+        throw new UnauthorizedError("Unauthorized");
+      }
 
+      return Bank.allBank;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  deleteBank(bankid) {
+    try {
+      let bankindex = Bank.findBankId(bankid);
+
+      Bank.allBank.splice(bankindex, 1);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  addAccount(bankId, accountInfo) {
+    try {
+      let indexOfBank = this.findBankId(bankId);
+
+      this.accounts.push(accountInfo);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  static findAccount(accountId) {
+    try {
+      if (typeof accountId != "number") {
+        throw new InvalidType("  invalid bankid");
+      }
+
+      for (let index = 0; index < this.accounts.length; index++) {
+        if (accountId == this.accounts[index].id) {
+          return index;
+        }
+      }
+      throw new NotFound("bank id Not found ");
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  static deleteAccount(bankId, accountId) {
+    try {
+      let indexofAccount = Bank.findAccount(accountId);
+
+      this.accounts.splice(indexofAccount, 1);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  static getBankDetail(bankid) {
+    try {
+      if (typeof bankid != "number") {
+        throw new InvalidType("  invalid bankid");
+      }
+
+      for (let index = 0; index < Bank.allBank.length; index++) {
+        if (bankid == Bank.allBank[index].id) {
+          return Bank.allBank[index];
+        }
+      }
+
+      throw new NotFound("bank id Not found ");
+    } catch (error) {
+      throw error;
+    }
   }
 }
-
-
 
 module.exports = Bank;
--- Bank/Transaction.js
+++ Bank/Transaction.js
@@ -1,63 +1,39 @@
 const UnauthorizedError = require("./Error/UnAuthorizedError");
-const NotFound=require("./Error/NotFound");
+const NotFound = require("./Error/NotFound");
 const InvalidType = require("./Error/InvalidType");
 
+class Transaction {
+  static id = 1;
+  constructor(date, transactionType, amount, balance) {
+    this.id = Transaction.id++;
+    this.date = date;
+    this.transactionType = transactionType;
+    this.amount = amount;
+    this.balance = balance;
+  }
 
-class Transaction{
-     static id=1
-    constructor( date,transactionType,amount,balance  )
-    {   
-        this.id=Transaction.id++
-        this.date=date
-        this.transactionType=transactionType
-        this.amount=amount
-        this.balance=balance
-    
-    }
+  getValue(parameter) {
+    try {
+      if (typeof parameter != "string") {
+        throw new InvalidType("invalid parameter format");
+      }
+      switch (parameter) {
+        case " id":
+          return this.id;
+        case " date":
+          return this.date;
+        case " transactionType":
+          return this.transactionType;
+        case " amount":
+          return this.amount;
+        case "balance":
+          return this.balance;
 
-    newTransation( transactionType,amount,balance)
-    {
-     
-          try{
-            if (typeof transactionType != "string") {
-                throw new InvalidType("Invalid transactionType");
-              }
-              if (typeof amount != "number") {
-                throw new InvalidType("Invalid amount");
-              }
-              if (typeof balance != "number") {
-                throw new InvalidType("Invalid balance");
-              }
-              if(transactionType.toLocaleLowerCase==="credit")
-              {
-                this.transactionType="credit";
-              }
-              else{
-                this.transactionType="debit";
-              }
-
-            let date=new Date();
-
-            new Transaction(date,transactionType,amount,balance)
-
-          
-          }
-          catch(error){
-            throw error
-          }
-
-
-
-
-
-
-
-    }
-
-
-    // updateTransaction()
-    
-
+        default:
+          throw new InvalidType("invalid parameter");
+      }
+    } catch (error) {}
+  }
 }
 
-module.exports= Transaction
+module.exports = Transaction;
